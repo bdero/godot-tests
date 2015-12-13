7569a89fca2b95e681ea82d8f82df2c8f9dd80fb
Brandon DeRosier <x@bdero.me>
2015-12-13 02:05:48 -0500

    Added movement to flycam

--- 2-flycam/player.gd
+++ 2-flycam/player.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 
+const SPEED = 5
+
 var camera
 var lastMousePos
 var currentMousePos = Vector2(0, 0)
@@ -11,20 +13,44 @@ var currentRotation = Vector2(0, 0)
 func _ready():
 	camera = get_node("camera")
 	set_process_input(true)
+	set_process(true)
+	
+func _process(delta):
+	movement(delta)
+	
+	currentRotation += (destinationRotation - currentRotation)*15*delta
+	
+	set_rotation(Vector3(0, currentRotation.x, 0))
+	camera.set_rotation(Vector3(currentRotation.y, 0, 0))
 	
 func _input(event):
 	if event.type==InputEvent.MOUSE_MOTION:
+		# Handle player rotation
 		lastMousePos = currentMousePos
 		currentMousePos = event.pos
+		
 		var diffMousePos = currentMousePos - lastMousePos
-		
 		destinationRotation -= diffMousePos/500
-		currentRotation += (destinationRotation - currentRotation)/5
-		
-		set_rotation(Vector3(0, currentRotation.x, 0))
-		camera.set_rotation(Vector3(currentRotation.y, 0, 0))
 	elif event.is_action_released("ui_cancel"):
 		get_tree().quit()
+		
+func movement(delta):
+	var transX = 0
+	var transZ = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		transZ -= 1
+	if Input.is_action_pressed("ui_down"):
+		transZ += 1
+	
+	if Input.is_action_pressed("ui_left"):
+		transX -= 1
+	if Input.is_action_pressed("ui_right"):
+		transX += 1
+	
+	var translation = Vector3(transX, 0, transZ).rotated(Vector3(0, -1, 0), currentRotation.x)
+	
+	set_translation(get_translation() + translation*SPEED*delta)
 	
 func _enter_tree():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
